33691816bfe01c7712f184385283721bcaba8e9c
prathul <prathul.mg@seqato.com>
2025-07-17 14:25:59 +0530

    Initial test

--- constants/index.ts
+++ constants/index.ts
@@ -346,15 +346,15 @@ export const FOOTER_DATA = [
 
 export const NAV_LINKS = [
   {
-    title: "About me",
+    title: "Home",
     link: "#about-me",
   },
   {
-    title: "Skills",
+    title: "Rewards",
     link: "#skills",
   },
   {
-    title: "Projects",
+    title: "Register",
     link: "#projects",
   },
 ] as const;
